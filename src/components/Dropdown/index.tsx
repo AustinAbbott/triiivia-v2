b619510505a4f6e9a34d69e5d0be2236af142a45
Austin Abbott <57454313+AustinAbbott@users.noreply.github.com>
2024-02-18 11:53:03 -0700

    Handle clicks outside of dropdown

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import "./style.css";
 
 type DropdownProps = {
@@ -10,22 +10,38 @@ type DropdownProps = {
 
 export const Dropdown: FC<DropdownProps> = (props) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const listRef = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+  }, []);
 
   const handleSelection = (option: any) => {
     props.setSelectedOption(option);
     setIsOpen(false);
   };
 
+  const handleOutsideClick = (e: Event) => {
+    if (e.target instanceof Element && !listRef.current?.contains(e.target)) {
+      setIsOpen(false);
+    }
+  };
+
   return (
-    <div className="drop-down-container">
-      <div>{props.label}</div>
-      <button onClick={() => setIsOpen(!isOpen)}>
-        {props.selectedOption || "Choose"}
+    <div className="drop-down-container" ref={listRef}>
+      <div className="drop-down-label">{props.label}</div>
+      <button className="drop-down-button" onClick={() => setIsOpen(!isOpen)}>
+        {props.selectedOption || "-- Choose --"}
       </button>
       <div className="dropdown-list">
         {isOpen &&
           props.options.map((option) => (
-            <button onClick={() => handleSelection(option)}>{option}</button>
+            <button
+              className="drop-down-button"
+              onClick={() => handleSelection(option)}
+            >
+              {option}
+            </button>
           ))}
       </div>
     </div>
