12f0808c8f4f0563616bf4fd0cf14c228855aafa
Austin Abbott <57454313+AustinAbbott@users.noreply.github.com>
2024-03-18 16:34:59 -0600

    Reduce over-engineered logic, add test

---
+++ src/views/QuestionCard/TrueOrFalse/TrueOrFalse.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { QuestionResponse } from "../../../constants";
+import TrueOrFalseCard from ".";
+
+describe("TrueOrFalse", () => {
+  const mockProps = {
+    questionData: { correct_answer: "True" } as QuestionResponse,
+    incrementIndex: jest.fn(),
+  };
+
+  test("mounts", () => {
+    render(<TrueOrFalseCard {...mockProps} />);
+    expect(screen.getByTestId("TrueOrFalseCard")).toBeTruthy();
+  });
+});
