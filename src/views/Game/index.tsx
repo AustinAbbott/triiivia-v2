af9954b52dd47c262d8e9f988bcb2299e0d2005a
Austin Abbott <57454313+AustinAbbott@users.noreply.github.com>
2024-04-03 17:56:31 -0600

    And the unused class name

--- src/views/Game/index.tsx
+++ src/views/Game/index.tsx
@@ -71,7 +71,7 @@ export const Game: FC<GameProps> = (props) => {
   ];
 
   return (
-    <div data-testid="Game" className="game">
+    <div data-testid="Game">
       {questionCards[cardIndex]}
       <button
         className="reset-game"
